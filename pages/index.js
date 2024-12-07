1d50568af6598b8dc0e89f202bd14e64bb318aca
Diogo <62947278+DiogoRC0102@users.noreply.github.com>
2024-12-07 15:18:31 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Lulu e Manu, eu amo vocês, se vocês me amam dêem uma risadinha!</h1>
+    return <h1>Lulu e Manu, eu amo vocês, se vocês me amam dêem uma risadinha! 🥰</h1>
 }
 
 export default Home;
